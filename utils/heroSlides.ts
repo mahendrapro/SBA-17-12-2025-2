8ce4c6e1b6e4bef016c64d6f9e54dcdf78bd436b
mahendrapro <mahendra0123456789@gmail.com>
2026-01-05 11:48:14 +0530

    Update heroSlides.ts

--- utils/heroSlides.ts
+++ utils/heroSlides.ts
@@ -1,1 +1,12 @@
+import { PROJECTS } from "../constants";
+
+export const HERO_SLIDES = PROJECTS
+  .filter(project => project.images && project.images.length > 0)
+  .map(project => ({
+    image: project.images![0], // hero.jpg
+    title: project.category
+      ? project.category.toUpperCase()
+      : "CONSTRUCTION PROJECT",
+    subtitle: project.name,
+  }));
 
